1eb258359bbc42709e946e21fd728ebbd2345a33
daveiator <43887102+daveiator@users.noreply.github.com>
2023-03-13 22:57:03 +0100

    added solaris option

--- static/js/dark.js
+++ static/js/dark.js
@@ -1,11 +1,14 @@
 var toggle = document.getElementById("dark-mode-toggle");
 var darkTheme = document.getElementById("dark-mode-theme");
+var solarisTheme = document.getElementById("solaris-mode-theme");
 
 toggle.addEventListener("click", () => {    
     if (toggle.className === "btn fas fa-moon") {
         setTheme("dark");
     } else if (toggle.className === "btn fas fa-sun") {
         setTheme("light");
+    } else if (toggle.className === "btn fas fa-yin-yang") {
+        setTheme("solaris");
     }
 });
 
@@ -13,13 +16,19 @@ function setTheme(mode) {
     localStorage.setItem("dark-mode-storage", mode);
     if (mode === "dark") {
         darkTheme.disabled = false;
+        solarisTheme.disabled = true;
         toggle.className = "btn fas fa-sun";
     } else if (mode === "light") {
         darkTheme.disabled = true;
+        solarisTheme.disabled = true;
+        toggle.className = "btn fas fa-yin-yang";
+    } else if (mode === "solaris") {
+        darkTheme.disabled = true;
+        solarisTheme.disabled = false;
         toggle.className = "btn fas fa-moon";
     }
 }
 
 // the default theme is light
-var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
+var savedTheme = localStorage.getItem("dark-mode-storage") || "solaris";
 setTheme(savedTheme);
